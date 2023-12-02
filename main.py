8329b9f8ad957105092c4c1318c941a1d2a2452d
Andrew Song <msong36@ucsc.edu>
2023-12-02 13:38:02 -0800

    putKvs done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from time import sleep
 import json
 from typing import Dict, Any
+from colorama import Fore, Back, Style
 
 FORWARDING_ADDRESS = os.getenv("FORWARDING_ADDRESS")
 FORWARD_URL = ""
@@ -13,6 +14,11 @@ peers = []
 vectorClock = [0]
 uniqueID = 0
 nextUID = 0
+
+green = Fore.GREEN
+blue = Fore.BLUE
+red = Fore.RED
+white = Fore.WHITE
 
 ############### VIEW ##############
 """
@@ -24,7 +30,7 @@ Sends the following fields to a ip with the method 'INITPACKAGE':
   kvs: dict
 """
 def sendInit(newReplicaIP: str, nextUniqueId: int, vectorClock: [int], keyStore):
-  print("in sendInit")
+  print(blue + 'Replica doing sendInit...' + white)
   print('serialize:', json.dumps(keyStore))
   payload = {
     'uniqueID': str(nextUniqueId),
@@ -41,6 +47,7 @@ def sendInit(newReplicaIP: str, nextUniqueId: int, vectorClock: [int], keyStore)
     response = requests.request('INITPACKAGE', newReplicaIP, json=jsonify(payload))
     sleep(1)
 
+  print(green + 'Replica finished sendInit\n' + white)
   return
 
 """
@@ -48,15 +55,15 @@ Received the method INITPACKAGE and sets current fields to received values
 """
 @app.route('/', methods=['INITPACKAGE'])
 def initSelf():
+  print(blue + 'Replica doing INITPACKAGE...' + white)
   global uniqueID, nextUID, vectorClock, kvs
-  checkglobals()
   payload = request.get_json()
-  uniqueID = payload.get('uniqueID')
-  nextUID = payload.get('nextUniqueID')
+  uniqueID = int(payload.get('uniqueID'))
+  nextUID = int(payload.get('nextUniqueID'))
   vectorClock = json.loads(payload.get('vectorClock'))
   kvs = json.loads(payload.get('keyStore'))
   peers.append('http://' + request.remote_addr + ':8090/')
-  checkglobals()
+  print(green + 'Replica finished INITPACKAGE' + white)
   return jsonify({"result": "success"}), 200
 
 """
@@ -64,29 +71,31 @@ Helper function that prints out global variables
 """
 def checkglobals():
   global uniqueID, nextUID, vectorClock, kvs
-  print("Starting globals check")
+  print(blue + "Starting globals check" + white)
   print("uniqueID:", uniqueID)
   print("nextUID:", nextUID)
   print("vectorClock:", vectorClock)
   print("kvs:", kvs)
-  print("Globals check finished")
+  print(blue + "Globals check finished" + white)
 
 """Returns jsonified peers"""
 @app.route('/view', methods=['GET'])
 def getView():
-  print("SELF IP:", request.url_root)
+  print(green + 'Replica did getView()')
   return jsonify({"view:": peers}), 200
 
 @app.route('/view', methods=['PUT'])
 def putView():
+  print(blue + "Replica received starting putView()" + white)
+
   global kvs, nextUID, vectorClock
   
   senderIP = 'http://' + request.remote_addr + ':8090/'
-  print("SENDERIP:", senderIP)
   payload = request.get_json()
   newReplicaIP = payload.get('socket-address')
 
   if newReplicaIP in peers:
+    print(green + 'Replica finished putView() with 200' + white)
     return jsonify({"result": "already present"}), 200
 
   vectorClock.append(0)
@@ -107,13 +116,12 @@ def putView():
       response = requests.request('INITPACKAGE', newReplicaIP, json=jsonify(payload))
     
     for peer in peers:
-      print("SENDER IP:", senderIP)
       response = requests.put(peer, json={'socket-address': senderIP})
       if response.status_code not in [200, 201]:
         retry.append(peer)
       
     while retry:
-      print("FAILED TO GET REPLY AFTER RETRY", retry)
+      print(red + "FAILED TO GET REPLY AFTER RETRY" + white)
       newRetry = []
       for peer in retry:
         response = requests.put(peer, newReplicaIP)
@@ -126,10 +134,12 @@ def putView():
 
   print(f"Added peer: {newReplicaIP}")
   print("Current peers:", peers)
-  return jsonify({"message": "Peer added successfully"}), 200
+  print(green + 'Replica finished putView with 201' + white)
+  return jsonify({"message": "Peer added successfully"}), 201
 
 @app.route('/view', methods=['DELETE'])
 def deleteView():
+  print(blue + 'Replica starting deleteView()' + white)
   senderIP = 'http://' + request.remote_addr + ':8090/'
   payload = request.get_json()
   targetReplicaIP = payload.get('socket-address')
@@ -139,13 +149,17 @@ def deleteView():
     print("Received order to remove self")
     global peers, vectorClock, uniqueID, nextUID, kvs
     peers = []
-    vectorClock = []
+    vectorClock = [0]
     uniqueID = 0
     nextUID = 1
     kvs = dict()
+    print(green + 'Replica finished deleteView by removing self with code 200' + white)
     return jsonify({"message": "Removed self"}), 200
   
   if targetReplicaIP not in peers:
+    print(type(targetReplicaIP))
+    print(peers)
+    print(red + 'Replica finished deleteView with 404' + white)
     return jsonify({"error": "View has no such replica"}), 404
   
   if senderIP not in peers:
@@ -166,33 +180,103 @@ def deleteView():
 
   peers.remove(targetReplicaIP)
 
+  print(green + 'Replica finished deleteView with code 200' + white)
   return jsonify({"result": "deleted"}), 200
 
 ############### KVS ##############
+def maxOutClock(vectorClockA, vectorClockB):
+  if len(vectorClockA) < len(vectorClockB):
+    vectorClockA += [0] * (len(vectorClockB) - len(vectorClockA))
+  elif len(vectorClockB) < len(vectorClockA):
+    vectorClockB += [0] * len(vectorClockB)
+  return max(vectorClockA, vectorClockB)
+
 @app.route('/kvs/<key>', methods=['PUT'])
-def putKvs(key):
+def putKvs(key: str):
+  print(blue + 'Replica starting putKvs()...' + white)
+  global vectorClock
+
   senderIP = 'http://' + request.remote_addr + ':8090/'
   payload = request.get_json()
-  key = payload.get('value')
+  value = str(payload.get('value'))
+
+  if not value:
+    print(red + 'Replica finished putKvs with 400' + white)
+    return jsonify({"error": "PUT request does not specify a value"}), 400
+  if len(value) > 100:
+    print(red + 'Replica finished putKvs with 400' + white)
+    return jsonify({"error": "Key is too long"}), 400
+
   casualMetadata  = payload.get('casual-metadata')
   print("Sender ip:", senderIP)
   print("Casual metadata:", casualMetadata)
   print("key:", key)
 
+  # if casualMetadata is not null:
   if type(casualMetadata) != type(None):
-    print("casualMetadata is not None")
-
+    print("Casual metadata is not null")
+    # Load casualMetadata as a list
     casualMetadata = json.loads(casualMetadata)
-    print("casualMetadata:", casualMetadata)
-    print("casualMetadata type:", type(casualMetadata))
-    print(vectorClock[uniqueID])
-    print(casualMetadata[uniqueID])
-    if vectorClock[uniqueID] < casualMetadata[uniqueID]:
-      return jsonify({"error": "Casual dependencies not satisfied; try again later"}), 503
-    print("It is of type:", type(casualMetadata))
-    print(casualMetadata)
+    casualMetadata = [1, 2, 3, 4, 5, 6, 7]
 
-  return jsonify({'temp key': key}), 200
+    if vectorClock[uniqueID] < casualMetadata[uniqueID]:
+      print(red + 'Replica finished putKvs with 503' + white)
+      return jsonify({"error": "Casual dependencies not satisfied; try again later"}), 503
+  else:
+    print("Casual metadata is null")
+    casualMetadata = [0] * len(vectorClock)
+
+  resultCode = 503
+  if key not in kvs:
+    resultCode = 201
+  else:
+    resultCode = 200
+
+  kvs[key] = value
+  print(vectorClock)
+  print(uniqueID)
+  vectorClock[uniqueID] += 1
+  vectorClock = maxOutClock(vectorClock, casualMetadata)
+
+  if senderIP not in peers:
+    retry = []
+    data = {
+      'value': value,
+      'casual-metadata': json.dumps(vectorClock),
+    }
+
+    for peer in peers:
+      print("PEER:", peer)
+      response = requests.put(peer + 'kvs/' + key, json=data)
+      if response.status_code == 503:
+        retry.append(peer)
+      elif response.status_code in [200, 201]:
+        responsePayload = response.json()
+        responseCM = json.loads(responsePayload['casual-metadata'])
+        vectorClock = maxOutClock(vectorClock, responseCM)
+        
+    while retry:
+      newRetry = []
+      data = {
+        'value': value,
+        'casual-metadata': json.dumps(vectorClock) 
+      }
+      for peer in retry:
+        response = requests.put(peer + 'kvs/' + key, json=data)
+        responsePayload = response.json()
+        responseCM = json.loads(responsePayload['casual-metadata'])
+        if response.status_code not in [200, 201]:
+          newRetry.append(peer)
+          vectorClock = maxOutClock(vectorClock, responseCM)
+      retry = newRetry
+
+  if resultCode == 200:
+    print(green + 'Replica finished putKvs() with 200' + white)
+    return jsonify({'result': 'created', 'casual-metadata': json.dumps(vectorClock)}), 200
+  elif resultCode == 201:
+    print(green + 'Replica finished putKvs() with 201' + white)
+    return jsonify({'result': 'replaced', 'casual-metadata': json.dumps(vectorClock)}), 201
+
 #@app.route('/kvs/<key>', methods=['GET', 'PUT', 'DELETE', 'CUSTOM'])
 '''def kvs(key):"
   print("in kvs")
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import requests
 import json
+from flask import jsonify
 
-'''url = 'http://10.0.0.242:8090/view'
-data = {'socket-address': 'http://10.0.0.242:8090'}
+url = 'http://10.0.0.242:8090/view'
+data = {'socket-address': 'http://10.0.0.242:8090/'}
 
 print("Attempting put of", url)
 response = requests.put(url, json=data)
@@ -13,14 +14,14 @@ response = requests.get(url)
 print(response)
 print(response.text)
 
-print("Attempting delete of", url)
-response = requests.delete(url, json={'socket-address': 'http://10.0.0.242:8090'})
+print("Attempting delete of", data.get('socket-address'))
+response = requests.delete(url, json={'socket-address': 'http://10.0.0.242:8090/'})
 print(response)
-print(response.text)'''
+print(response.text)
 
 print("Starting kvs tests")
 url = 'http://10.0.0.242:8090/kvs/'
-cm = json.dumps([0, 0, 0])
+cm = None
 
 keys = ['apple', 'banana', 'pear']
 values = ['zero', 'one', 'two']
@@ -29,10 +30,11 @@ data = {'casual-metadata': cm}
 print("data: ", data)
 
 for i, key in enumerate(keys):
-  data = {'casual-metadata': cm, 'key': i}
+  data = {'casual-metadata': cm, 'value': i}
   response = requests.put(url + key, json=data)
   print(response)
   print(response.text)
+  payload = response.json()
 
 '''response = requests.put('http://1234.1234.1234.1234', json=data)
 print(response)
